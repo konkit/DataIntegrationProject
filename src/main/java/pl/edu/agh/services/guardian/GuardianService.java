793bee93ecfc00c5127fa7f4e80c3aaacdaabcf4
Slawex <slawomirbukowiec@gmail.om>
2016-05-22 02:25:28 +0200

    Fix for downloading Guardian articles: - correct the address - iterate over pages

--- src/main/java/pl/edu/agh/services/guardian/GuardianService.java
+++ src/main/java/pl/edu/agh/services/guardian/GuardianService.java
@@ -13,6 +13,8 @@ import twitter4j.User;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -29,6 +31,18 @@ public class GuardianService {
     @Value("${guardian.api.endpoint}")
     private String apiEndpoint;
 
+    @Value("${guardian.search}")
+    private String apiSearch;
+
+    @Value("${guardian.show_fields}")
+    private String showFields;
+
+    @Value("${guardian.show_tags}")
+    private String showTags;
+
+    @Value("${guardian.page}")
+    private String page;
+
     static Logger logger = LoggerFactory.getLogger(GuardianService.class);
 
     @Autowired
@@ -41,59 +55,78 @@ public class GuardianService {
         try {
             BlockingQueue<String> queue = new LinkedBlockingQueue<>(10000);
 
-            String address = new String(apiEndpoint + "&api-key=" + key);
-
-            URL url = new URL(address);
-            Scanner scanner = new Scanner(url.openStream());
-            String str = new String();
-            while(scanner.hasNext()) {
-                str += scanner.nextLine();
+            String guardianTagsStr = "";
+            String[] splittedGuardianTags = search.getGuardianTags().split("\\s*,\\s*");
+            for(String tag : splittedGuardianTags) {
+                guardianTagsStr += tag + "%2C%20";
             }
-            scanner.close();
 
-            JSONObject jsonObject = new JSONObject(str);
-            JSONObject jsonResponse = jsonObject.getJSONObject("response");
-            JSONArray resultsArray = jsonResponse.getJSONArray("results");
+            if(splittedGuardianTags.length != 0) {
+                guardianTagsStr = guardianTagsStr.substring(0, guardianTagsStr.length() - 6);
+            }
 
-            for(int i = 0; i < resultsArray.length(); i++) {
-                try {
-                    JSONObject singleResult = resultsArray.getJSONObject(i);
+            int page_num = 1;
+            int pages = 1;
 
-                    JSONObject fields = singleResult.getJSONObject("fields");
+            while(page_num <= pages) {
+                String address = new String(apiEndpoint + apiSearch + showTags + showFields + page + page_num + "&q=" + guardianTagsStr + "&api-key=" + key);
 
-                    GuardianArticle article = new GuardianArticle();
-
-                    article.setWebTitle(singleResult.getString("webTitle"));
-                    article.setApiUrl(singleResult.getString("apiUrl"));
-                    article.setWebUrl(singleResult.getString("webUrl"));
-                    article.setSectionName(singleResult.getString("sectionName"));
-                    article.setPublicationDate(singleResult.getString("webPublicationDate"));
-                    //getting body (content) of the article
-                    article.setText(fields.getString("body"));
-
-                    article.setSearch(search);
-
-                    //getting author (contributor) of the article
-                    JSONArray tags = singleResult.getJSONArray("tags");
-                    JSONObject contributorTag = null;
-
-                    for(int j = 0; j < tags.length(); j++) {
-                        JSONObject singleTag = tags.getJSONObject(j);
-                        if(singleTag.getString("type") == "contributor") {
-                            contributorTag = singleTag;
-                            break;
-                        }
-                    }
-
-                    if(contributorTag != null) {
-                        Author author = getAuthor(contributorTag);
-                        article.setAuthor(author);
-                    }
-                    guardianRepository.save(article);
-                } catch( DataIntegrityViolationException e ) {
-                    System.out.println(e.getMessage());
+                URL url = new URL(address);
+                Scanner scanner = new Scanner(url.openStream());
+                String str = new String();
+                while(scanner.hasNext()) {
+                    str += scanner.nextLine();
                 }
+                scanner.close();
+
+                JSONObject jsonObject = new JSONObject(str);
+                JSONObject jsonResponse = jsonObject.getJSONObject("response");
+                pages = jsonResponse.getInt("pages");
+
+                JSONArray resultsArray = jsonResponse.getJSONArray("results");
+
+                for(int i = 0; i < resultsArray.length(); i++) {
+                    try {
+                        JSONObject singleResult = resultsArray.getJSONObject(i);
+
+                        JSONObject fields = singleResult.getJSONObject("fields");
+
+                        GuardianArticle article = new GuardianArticle();
+                        article.setWebTitle(singleResult.getString("webTitle"));
+                        article.setApiUrl(singleResult.getString("apiUrl"));
+                        article.setWebUrl(singleResult.getString("webUrl"));
+                        article.setSectionName(singleResult.getString("sectionName"));
+                        article.setPublicationDate(singleResult.getString("webPublicationDate"));
+                        //getting body (content) of the article
+                        article.setText(fields.getString("body"));
+
+                        article.setSearch(search);
+
+                        //getting author (contributor) of the article
+                        JSONArray tags = singleResult.getJSONArray("tags");
+                        JSONObject contributorTag = null;
+
+                        for(int j = 0; j < tags.length(); j++) {
+                            JSONObject singleTag = tags.getJSONObject(j);
+                            if(singleTag.getString("type").equals("contributor")) {
+                                contributorTag = singleTag;
+                                break;
+                            }
+                        }
+
+                        if(contributorTag != null) {
+                            Author author = getAuthor(contributorTag);
+                            article.setAuthor(author);
+                        }
+                        guardianRepository.save(article);
+                    } catch( DataIntegrityViolationException e ) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+
+                page_num++;
             }
+
         } catch(MalformedURLException e) {
             logger.error("Error fetching from Guardian!!!" + e.getMessage());
             e.printStackTrace();
@@ -106,13 +139,23 @@ public class GuardianService {
     private Author getAuthor(JSONObject contributorTag) {
 
         String guardianName = contributorTag.getString("webTitle");
-        String twitterName = contributorTag.getString("twitterHandle");
+        String twitterName = "";
+
+        try {
+            twitterName = contributorTag.getString("twitterHandle");
+        }
+        catch(Exception ex) {
+
+        }
 
         Author author = authorRepository.findByGuardianName(guardianName);
 
         //byc moze ma konto na Twitterze
         if(author == null) {
-            author = authorRepository.findByTwitterName(twitterName);
+            if(twitterName != "") {
+                author = authorRepository.findByTwitterName(twitterName);
+            }
+
 
             if(author != null) {
                 author.setGuardianName(guardianName);
